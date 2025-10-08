4cbf4add718b747058cb37b5e3b404548fa5ee23
denopebble-tech <denopebble@gmail.com>
2025-10-08 18:38:36 +0300

    Add files via upload

---
+++ first.py
@@ -0,0 +1,9 @@
+print("Hello I am an AI chatbot what is your name: ")
+Name = input()
+print ("nice to meet you", Name )
+Mood = input().lower()
+if Mood == "good": 
+    print("happy to hear that!")
+else : 
+    print(" sorry to hear that...")
+    
