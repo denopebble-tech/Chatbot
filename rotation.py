ce438bd7416e8f522b3cf45a5b46335643247083
denopebble-tech <denopebble@gmail.com>
2025-11-26 18:36:44 +0300

    Add files via upload

---
+++ rotation.py
@@ -0,0 +1,21 @@
+import cv2 
+import numpy as np
+import matplotlib.pyplot as plt
+
+image = cv2.imread("dog.avif")
+image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+(h, w) = image.shape[:2]
+center = (w//2, h//2)
+M = cv2.getRotationMatrix2D(center, 45, 1.0 )
+rotated = cv2.warpAffine(image, M, (w,h))
+rotated_rgp = cv2.cvtColor(rotated,cv2.COLOR_BGR2RGB)
+plt.imshow(rotated_rgp)
+plt.title("Rotated Image")
+plt.show()
+
+brightness_matrix = np.ones(image.shape, dtype="uint8") *50
+brighter = cv2.add(image,brightness_matrix)
+brighter_rgb = cv2.cvtColor(brighter, cv2.COLOR_BAYER_BG2BGR)
+plt.imshow(brighter_rgb)
+plt.title("Brighter image")
+plt.show
